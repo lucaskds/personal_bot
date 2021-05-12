d9b54920e29ec7ef2707fb3ec91dda81a8bbd3fd
Lucas Krämer de Sousa <lucas.kramer.sousa@gmail.com>
2021-05-12 20:13:22 -0300

    :sparkles: 🚔

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const app = express();
 const router = express.Router();
 
 app.use('/', router.get('/', (req, res) => res.status(200).json({
-    message: 'Welcome to Where To Stay API!',
+    message: 'Welcome to Personal BOT!',
 })));
 
 module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-console */
 const mongoose = require('mongoose');
+const got = require('got');
 const app = require('./app');
 const bot = require('./bot');
 
@@ -19,5 +20,17 @@ const MONGO_URL = process.env.DATABASE_CONNECTION_STRING_LOCAL || 'mongodb://db:
 //     });
 bot.launch();
 app.listen(PORT);
+
+const pingInMe = () => {
+    got.get(process.env.APP_URL, {responseType: 'json'})
+    .then(res => {
+        setTimeout(() => {  pingInMe() }, parseInt(Math.random() * 10 + 1) * 60000);
+    })
+    .catch(err => {
+        console.log('Error: ', err.message);
+    });
+}
+
+pingInMe();
 
 console.log('Personal BOT running!');
