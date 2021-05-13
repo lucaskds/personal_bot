b3a120e94ba21d78bde1e5c9324af0684d171c58
Lucas Krämer de Sousa <lucas.kramer.sousa@gmail.com>
2021-05-12 21:11:19 -0300

    :feat: Handle non participants - Avoid crashing on non participants - Support MarkdownV2

--- src/bot.js
+++ src/bot.js
@@ -16,18 +16,28 @@ const dashboard = async(ctx) => {
     for (participant of Object.values(mapping)) {
         participants.push(await codeWarsClient.getUserInfo(participant))
     }
-    response = participants.map((participant) => {
-        return `${participant.username} | Honor: ${participant.honor} | Rank: ${participant.rank} | Completed: ${participant.completed}`
+    const sortedParticipants = participants.sort((a, b) => {
+        if (a.honor < b.honor) return 1
+        if (a.honor > b.honor) return -1
+        return 0
     })
-    ctx.reply(`DASHBOARD\n${response.join("\n")}`)
+    response = sortedParticipants.map((participant) => {
+        username = participant.username.replace(/([^a-zA-Z0-9])/, (c) => `\\${c}`)
+        return `👨‍💻 ${username}\n🏆 ${participant.honor}  🥋 ${participant.rank}  ✅ ${participant.completed}\n`
+    })
+    ctx.replyWithMarkdownV2(`📊 __*DASHBOARD*__ 📊\n\n${response.join("\n")}`)
 }
 
 const mystatus = async(ctx) => {
     const from = ctx.update.message.from
     const mapping = JSON.parse(process.env.PARTICIPANTS_MAPPING)
     participant = mapping[from.username]
+    if (!participant) {
+        ctx.replyWithMarkdownV2(`Opa ${from.first_name}, parece que você não está participando do desafio\\.\\.\\. 😢\n[Lucas](tg://user?id=${process.env.MY_USER_ID}) corre aqui\\! 🏃‍♂️`)
+        return
+    }
     const cw_user = await codeWarsClient.getUserInfo(participant)
-    ctx.reply(`${participant}\nHonor: ${cw_user.honor}\nRank: ${cw_user.rank}\nCompleted: ${cw_user.completed}`)
+    ctx.replyWithMarkdownV2(`🏅 __*${participant}*__ 🏅\n🏆 _*Honor:*_ _${cw_user.honor}_\n🥋 _*Rank:*_ _${cw_user.rank}_\n✅ _*Completed:*_ _${cw_user.completed}_`)
 }
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
